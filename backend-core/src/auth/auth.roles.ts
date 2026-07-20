7b0f6d038984f83bc97303b28f696806ddd00057
Mrtindanzor <ktindanzor@gmail.com>
2026-07-20 03:53:17 +0000

    Fixed roles errors in backend core

--- backend-core/src/auth/auth.roles.ts
+++ backend-core/src/auth/auth.roles.ts
@@ -1,13 +1,14 @@
 import type { IUserAccount } from "../user/user.contracts.types";
 
 export function roleBuilder<
-	TUser extends IUserAccount,
-	Roles extends TUser["roles"] = TUser["roles"],
+  TUser extends IUserAccount,
+  Roles extends TUser["roles"] = TUser["roles"],
 >(userRoles: Roles, requiredRoles: Roles[number][]) {
-	return {
-		add: (role: Roles[number]) => {
-			return roleBuilder(userRoles, [...requiredRoles, role]);
-		},
-		passes: () => requiredRoles.every((role) => userRoles.includes(role)),
-	};
+  return {
+    add: (role: Roles[number]) => {
+      return roleBuilder(userRoles, [...requiredRoles, role]);
+    },
+    passes: () =>
+      requiredRoles.every((role) => (userRoles ?? []).includes(role)),
+  };
 }
