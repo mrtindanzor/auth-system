f51b6ca426974f77eeafa24ed960a9c9b609b840
Mrtindanzor <ktindanzor@gmail.com>
2026-07-20 08:16:46 +0000

    Fixed: role builder error in frontend

--- frontend-core/src/auth/store.ts
+++ frontend-core/src/auth/store.ts
@@ -4,53 +4,55 @@ import { roleBuilder } from "../utils/auth.roles";
 import { decodeUserFromToken } from "./tokens";
 
 export type AuthState = {
-	accessToken: string | null;
-	isLoggedIn: boolean;
-	hasRefreshed: boolean;
+  accessToken: string | null;
+  isLoggedIn: boolean;
+  hasRefreshed: boolean;
 };
 
 export type AuthActions<
-	TUser extends IUserAccount,
-	TRoles extends TUser["roles"] = TUser["roles"],
+  TUser extends IUserAccount,
+  TRoles extends TUser["roles"] = TUser["roles"],
 > = {
-	setAccessToken: (accessToken: string | null) => void;
-	getAccessToken: () => string | null;
-	logout: () => void;
-	roles: TRoles;
-	roleChecker: ReturnType<RoleChecker<TUser>>;
+  setAccessToken: (accessToken: string | null) => void;
+  getAccessToken: () => string | null;
+  logout: () => void;
+  roles: TRoles;
+  roleChecker: ReturnType<RoleChecker<TUser>>;
 };
 
 export type AuthStore<TUser extends IUserAccount> = AuthState &
-	AuthActions<TUser>;
+  AuthActions<TUser>;
 
 export function createAuthStore<TUser extends IUserAccount>() {
-	return create<AuthStore<TUser>>((set, get) => ({
-		accessToken: null,
-		isLoggedIn: false,
-		hasRefreshed: false,
+  return create<AuthStore<TUser>>((set, get) => ({
+    accessToken: null,
+    isLoggedIn: false,
+    hasRefreshed: false,
 
-		setAccessToken(accessToken) {
-			const roles = decodeUserFromToken<TUser>(accessToken)?.roles;
+    setAccessToken(accessToken) {
+      let roles = decodeUserFromToken<TUser>(accessToken)?.roles;
+      if (!Array.isArray(roles)) roles = [];
 
-			set({
-				isLoggedIn: !!accessToken,
-				accessToken,
-				hasRefreshed: true,
-				roles: Array.isArray(roles) ? roles : [],
-			});
-		},
+      set({
+        isLoggedIn: !!accessToken,
+        accessToken,
+        hasRefreshed: true,
+        roles,
+        roleChecker: roleBuilder(roles, []),
+      });
+    },
 
-		getAccessToken() {
-			return get().accessToken;
-		},
+    getAccessToken() {
+      return get().accessToken;
+    },
 
-		logout() {
-			set({
-				accessToken: null,
-				isLoggedIn: false,
-			});
-		},
-		roles: [],
-		roleChecker: roleBuilder(get().roles, []),
-	}));
+    logout() {
+      set({
+        accessToken: null,
+        isLoggedIn: false,
+      });
+    },
+    roles: [],
+    roleChecker: roleBuilder<TUser>([], []),
+  }));
 }
--- frontend-core/src/test/test.App.tsx
+++ frontend-core/src/test/test.App.tsx
@@ -2,55 +2,62 @@ import { useEffect } from "react";
 import { createAuthClient } from "../index";
 
 type User = {
-	email: string;
-	name: string;
-	roles: ("developer" | "admin" | "user" | "guest")[];
+  email: string;
+  name: string;
+  roles: ("developer" | "admin" | "user" | "guest")[];
 };
 type LoginProps = { email: string; password: string };
 type Register = { email: string; name: string; password: string };
 
 const { useAuthRefresh, useAuthStore } = createAuthClient<
-	User,
-	LoginProps,
-	Register
+  User,
+  LoginProps,
+  Register
 >(
-	{
-		baseUrl: "http://localhost:8000",
-		endpoints: {
-			login: {
-				method: "post",
-				url: "/auth/login",
-			},
-			refresh: {
-				method: "get",
-				url: "/auth/refresh",
-			},
-			register: {
-				method: "post",
-				url: "/auth/register",
-			},
-			logout: {
-				method: "post",
-				url: "/auth/logout",
-			},
-		},
-	},
-	{
-		authPaths: [],
-		protectedPaths: [],
-		onAuthenticated(_currentPath) {},
-		onUnauthenticated(_currentPath) {},
-	},
+  {
+    baseUrl: "http://localhost:8000/fbci",
+    endpoints: {
+      login: {
+        method: "post",
+        url: "/auth/login",
+      },
+      refresh: {
+        method: "get",
+        url: "/auth/refresh",
+      },
+      register: {
+        method: "post",
+        url: "/auth/register",
+      },
+      logout: {
+        method: "post",
+        url: "/auth/logout",
+      },
+    },
+  },
+  {
+    authPaths: [],
+    protectedPaths: [],
+    onAuthenticated(_currentPath) {},
+    onUnauthenticated(_currentPath) {},
+  },
 );
 
 export function App() {
-	useAuthRefresh();
-	const hasRefreshed = useAuthStore((s) => s.hasRefreshed);
-	const isLoggedIn = useAuthStore((s) => s.isLoggedIn);
+  useAuthRefresh();
+  const hasRefreshed = useAuthStore((s) => s.hasRefreshed);
+  const isLoggedIn = useAuthStore((s) => s.isLoggedIn);
+  const roleChecker = useAuthStore((s) => s.roleChecker);
+  const isDeveloper = roleChecker.add("developer").passes();
+  const isAdmin = roleChecker.add("admin").passes();
+  const roles = useAuthStore((s) => s.roles);
+  console.log(roles);
 
-	useEffect(() => {
-		console.log({ isLoggedIn, hasRefreshed });
-	}, [isLoggedIn, hasRefreshed]);
+  console.log({ isDeveloper, isAdmin });
 
-	return <div> works</div>;
+  useEffect(() => {
+    console.log({ isLoggedIn, hasRefreshed });
+  }, [isLoggedIn, hasRefreshed]);
+
+  return <div> works</div>;
 }
