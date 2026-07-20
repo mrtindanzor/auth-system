35e8227d79e5c9837d0f144c51d90e7f9c5a7563
Mrtindanzor <ktindanzor@gmail.com>
2026-07-20 09:30:54 +0000

    formatted with biome

--- backend-core/src/index.ts
+++ backend-core/src/index.ts
@@ -2,57 +2,57 @@ import type { ISigninProps, ISignupProps } from "./auth/auth.contracts.types";
 import { AuthService } from "./auth/auth.service";
 import type { AuthSecretsConfig } from "./config";
 import type {
-  IUserAccount,
-  IUserRepository,
+	IUserAccount,
+	IUserRepository,
 } from "./user/user.contracts.types";
 import { UserService } from "./user/user.service";
 import { getBearerToken } from "./utils/getBearerToken";
 
 export type { IAuthService } from "./auth/auth.contracts.types";
 export { AuthService } from "./auth/auth.service";
-export { createAuthConfig } from "./config";
 export type { AuthSecretsConfig } from "./config";
-export {
-  clearAuthCookie,
-  createAuthCookie,
-} from "./cookie";
+export { createAuthConfig } from "./config";
 export type {
-  AuthCookieOptions,
-  ClearCookieResult,
-  SetCookieResult,
+	AuthCookieOptions,
+	ClearCookieResult,
+	SetCookieResult,
 } from "./cookie";
 export {
-  AppError,
-  ForbiddenError,
-  NotFoundError,
-  UnauthorizedError,
-  ValidationError,
+	clearAuthCookie,
+	createAuthCookie,
+} from "./cookie";
+export {
+	AppError,
+	ForbiddenError,
+	NotFoundError,
+	UnauthorizedError,
+	ValidationError,
 } from "./errors";
 export type {
-  IUserAccount,
-  IUserService,
+	IUserAccount,
+	IUserService,
 } from "./user/user.contracts.types";
 export type { IUserRepository };
 
 export type AuthenticationServiceProps<TUser extends IUserAccount> = {
-  userRepo: IUserRepository<TUser>;
-  secretsConfig: AuthSecretsConfig;
+	userRepo: IUserRepository<TUser>;
+	secretsConfig: AuthSecretsConfig;
 };
 
 export function createAuthenticationService<
-  TUser extends IUserAccount,
-  TSignupProps extends ISignupProps<TUser>,
-  TSigninProps extends ISigninProps,
+	TUser extends IUserAccount,
+	TSignupProps extends ISignupProps<TUser>,
+	TSigninProps extends ISigninProps,
 >({ userRepo, secretsConfig }: AuthenticationServiceProps<TUser>) {
-  const userService = new UserService<TUser>(userRepo);
-  const authService = new AuthService<TUser, TSignupProps, TSigninProps>(
-    userService,
-    secretsConfig,
-  );
+	const userService = new UserService<TUser>(userRepo);
+	const authService = new AuthService<TUser, TSignupProps, TSigninProps>(
+		userService,
+		secretsConfig,
+	);
 
-  return {
-    userService,
-    authService,
-    getBearerToken,
-  };
+	return {
+		userService,
+		authService,
+		getBearerToken,
+	};
 }
